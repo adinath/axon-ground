f21b6c1df2f45a4a11ad68f18e40d6a430f346bc
adinath <adinaths@gmail.com>
2015-12-21 09:49:37 +0530

    add get user endpoint to view user by user id

--- src/main/java/fun/query/repository/UserRepository.java
+++ src/main/java/fun/query/repository/UserRepository.java
@@ -5,5 +5,8 @@ import fun.query.views.UserView;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<UserView, String> {
+
 }
--- src/main/java/fun/web/UserController.java
+++ src/main/java/fun/web/UserController.java
@@ -4,6 +4,8 @@ package fun.web;
 import fun.commands.CreateUserCommand;
 import fun.configuration.MyFeatures;
 import fun.domains.model.User;
+import fun.query.repository.UserRepository;
+import fun.query.views.UserView;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.springframework.http.ResponseEntity.badRequest;
+import static org.springframework.http.ResponseEntity.notFound;
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
@@ -21,6 +24,9 @@ public class UserController {
 
     @Autowired
     CommandGateway commandGateway;
+
+    @Autowired
+    UserRepository users;
 
     @RequestMapping(value = "/api/user", method = RequestMethod.GET)
     @ResponseBody
@@ -32,10 +38,22 @@ public class UserController {
     @ResponseBody
     public ResponseEntity<?> registerUser(@RequestBody String userName) {
         if (MyFeatures.CAN_CREATE_USER.isActive()) {
-            commandGateway.send(new CreateUserCommand(UUID.randomUUID().toString(),userName));
-            return ok().build();
+            UUID userId = UUID.randomUUID();
+            commandGateway.send(new CreateUserCommand(userId.toString(),userName));
+            return ok().body(userId);
         } else {
             return badRequest().build();
+        }
+    }
+
+    @RequestMapping(value = "/api/user/{userId}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity getUser(@PathVariable String userId){
+        UserView user = users.findOne(userId);
+        if(user == null){
+            return notFound().build();
+        }else{
+            return ok().body(user);
         }
     }
 }
